cbd6f5784bd29e7a27e146dff61491a79aaea515
PascalRomb <pasquale.rombola97@gmail.com>
2020-07-18 18:14:33 +0200

    samplePlugins updated

--- main-app/src/app/plugins/utils/samplePlugins.ts
+++ main-app/src/app/plugins/utils/samplePlugins.ts
@@ -1,9 +1,9 @@
 import { MyPlugin } from "./Plugin";
 
 export const samplePlugins : MyPlugin [] = [
-    new MyPlugin("http://localhost:8081", "header", "header"), //rxjs && httpModule
-    new MyPlugin("http://localhost:8082", "carousel"), //boostrap carousel
-    new MyPlugin("http://localhost:8081", "grid"), //kendo grid
+    new MyPlugin("http://localhost:8081", "header-plugin", "header"), //rxjs && httpModule
+    new MyPlugin("http://localhost:8082", "carousel-plugin"), //boostrap carousel
+    new MyPlugin("http://localhost:8083", "grid-plugin"), //kendo grid
 ]
 
 
